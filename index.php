2e2ecb566b18297002fbf44ea1fe0e14af13cec0
superkelvint <superkelvint@gmail.com>
2012-11-14 11:45:39 -0800

    Removed commented out nav bar

--- index.php
+++ index.php
@@ -2,45 +2,7 @@
 $page_title="Apache Solr vs ElasticSearch - the Feature Smackdown!";
 include_once("inc/header.php");
 ?>
-<? /*
-<div class="navbar navbar-inverse navbar-fixed-top">
-  <div class="navbar-inner">
-      <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-              <ul class="nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#contact">Contact</a></li>
-                  <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                      <ul class="dropdown-menu">
-                          <li><a href="#">Action</a></li>
-                          <li><a href="#">A<img src="img/cross.png">ther action</a></li>
-                          <li><a href="#">Something else here</a></li>
-                          <li class="divider"></li>
-                          <li class="nav-header">Nav header</li>
-                          <li><a href="#">Separated link</a></li>
-                          <li><a href="#">One more separated link</a></li>
-                      </ul>
-                  </li>
-              </ul>
-              <form class="navbar-form pull-right"><!--
-                  <input class="span2" type="text" placeholder="Email">
-                  <input class="span2" type="password" placeholder="Password">
-                  <button type="submit" class="btn">Sign in</button>
-                  -->
-              </form>
-          </div><!--/.nav-collapse -->
-      </div>
-  </div>
-</div>
-*/?>
+
 <div class="container">
   <div class="jumbotron subhead">
       <h1 class="secthead" style="margin-top:-10px;margin-bottom:-5px">Apache Solr vs ElasticSearch</h1><h3 class="secthead">The Feature Smackdown</h3>
